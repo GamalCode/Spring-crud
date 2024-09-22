a4c37d4ad9c9d918b373456354f6e55bf4e2d447
GamalCode <66071550+GamalCode@users.noreply.github.com>
2024-09-23 01:44:26 +0200

    Add files via upload

---
+++ src/main/java/com/example/demo/student/Student.java
@@ -0,0 +1,43 @@
+package com.example.demo.student;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity //Mapping Student Class to DB
+@Table
+public class Student {
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
+    private Long id;
+    private String name;
+    private String email;
+    private LocalDate dob;
+    @Transient //Won't be visible in the column of table
+    private Integer age;
+
+    public Student(String name, String email, LocalDate dob) {
+        this.name = name;
+        this.email = email;
+        this.dob = dob;
+    }
+
+    public Integer getAge() {
+        return Period.between(this.dob, LocalDate.now()).getYears();
+    }
+}
